55e6401d38cf7e1f34c98a1b8aedcdc89bcc6bf6
Alok Mishra <111239158+alokmishra1210@users.noreply.github.com>
2022-10-06 18:34:06 -0400

    Time: 44 ms (75.22%), Space: 13.9 MB (12.03%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,22 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry=0
+        mx_len=len(digits)-1
+        temp_n=(int(digits[mx_len])+1)+carry
+        carry=temp_n//10
+        tmp=temp_n%10
+        digits[mx_len]=tmp
+        mx_len=mx_len-1
+        if carry==1 and mx_len>=0:
+            while carry!=0 and mx_len>=0:
+                temp_n=int(digits[mx_len])+carry
+                carry=temp_n//10
+                tmp=temp_n%10
+                digits[mx_len]=tmp
+                mx_len=mx_len-1
+        if carry==1:
+            digits.insert(0,carry)
+        
+        return digits
+                
+    
